2fa176ee90062e5d1c9fde70760d3e88ed9d4901
er-priyanka <shahanepriyankas01@gmail.com>
2024-09-14 13:06:49 +0530

    added transition to answer section dynamically

--- frontend/src/Components/QuestionAndAnswer.jsx
+++ frontend/src/Components/QuestionAndAnswer.jsx
@@ -1,13 +1,24 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { MdAdd, MdMinimize } from 'react-icons/md';
 
 const QuestionAndAnswer = ({data}) => {
     const [showAnswer, setShowAnswer] = useState(false);
-
+    const [divHeight, setDivHeight] = useState(0);
+    const divRef = useRef(null);
     
     const handleAnswer = ()=>{
-        setShowAnswer(!showAnswer); // toggle the answer visibility
+        setShowAnswer((prev) => !prev); // toggle the answer visibility
     }
+
+    useEffect(()=>{
+        if(showAnswer && divRef.current){
+            // set height dynamically when answer is showing
+            setDivHeight(divRef.current.scrollHeight); 
+        } else {
+            // collapse the div when hidden
+            setDivHeight(0);
+        }
+    }, [showAnswer]);
     
   return (
     <div className='text-left '>
@@ -20,6 +31,7 @@ const QuestionAndAnswer = ({data}) => {
         border
         font-semibold
         text-sky-950
+        rounded shadow
         ' 
         onClick={handleAnswer}>
             <p>{data.que}</p>
@@ -30,15 +42,22 @@ const QuestionAndAnswer = ({data}) => {
         {/* Answer section */}
         <div 
         className='
+        transition-all duration-500 ease-in-out overflow-hidden
         p-[10px]
         border
         font-semibold
         text-sky-800'
         style={{
-            display:showAnswer?"block":"none"
-            }}
-            >
+            display:showAnswer?"block":"none",
+            height: `${divHeight}px` // Dynamically set height based on state
+        }}
+
+        ref={divRef}
+        >
+            <div className='mt-2'>
             <p>{data.ans}</p>
+            </div>
+            
         </div>
     </div>
   )
