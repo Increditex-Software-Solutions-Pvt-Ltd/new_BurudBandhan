fe1969fa527b512a4bbde8bc3582dad9c2fa04c5
er-priyanka <shahanepriyankas01@gmail.com>
2024-09-23 15:56:31 +0530

    backend connected with db

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+require('dotenv').config();
+const { connection } = require("./Config/db");
+
+const PORT = process.env.PORT;
 
 const app = express();
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello'));
 
-app.listen(8080, () => {console.log('server started on http://localhost:8080')});
+app.listen(PORT, async() => {
+    try{
+        await connection;
+        console.log('Connected with db');
+    }catch(err){
+        console.log(err);
+    }
+
+    console.log(`server started on http://localhost:${PORT}`)
+});
