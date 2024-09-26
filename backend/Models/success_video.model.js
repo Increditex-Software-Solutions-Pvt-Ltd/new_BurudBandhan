fb05e565c5a88e638dce55a46bb2d4d7534f5e11
er-priyanka <shahanepriyankas01@gmail.com>
2024-09-26 11:12:31 +0530

    modified success video schema

--- backend/Models/success_video.model.js
+++ backend/Models/success_video.model.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose');
 const successVideoSchema = new mongoose.Schema({
     url:{type:String, required:true},
     year: {type:String, required:true},
-    description: {type:String, required:true}
-}) 
+    description: {type:String, required:true},
+    user: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+    }
+}); 
 
 
 const SuccessVideo = mongoose.model('SuccessVideo', successVideoSchema);
