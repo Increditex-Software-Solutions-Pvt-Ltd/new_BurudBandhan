b80911da1ede5a65e1cf90f74be1066c78a7c654
er-priyanka <shahanepriyankas01@gmail.com>
2024-09-28 17:55:46 +0530

    created controller for members

---
+++ backend/Controllers/member.controller.js
@@ -0,0 +1,20 @@
+const Member = require('../Models/member.model');
+
+
+const memberController = {
+    async addMember(req, res){
+        // add new member
+    },
+    async getAllMembers(req, res){
+        // get all members
+    },
+    async updateMembers(req, res){
+        // update required details about the members
+    },
+    async deleteMember(req, res){
+        // delete member
+    }
+}
+
+
+module.exports = memberController;
