5e074b1ba88748e79a261a7088f558be6cec4591
er-priyanka <shahanepriyankas01@gmail.com>
2024-09-28 18:22:07 +0530

    admin can add new member

--- backend/Controllers/member.controller.js
+++ backend/Controllers/member.controller.js
@@ -4,6 +4,19 @@ const Member = require('../Models/member.model');
 const memberController = {
     async addMember(req, res){
         // add new member
+        const {memberImage, fullName, position} = req.body;
+
+        try{
+            const newMember = new Member({memberImage, fullName, position});
+            await newMember.save();
+            res.status(201).json({
+                message: "New Member Added successfully!",
+                member:newMember
+            });
+
+        }catch(err){
+            res.status(500).json({message: err.message});
+        }
     },
     async getAllMembers(req, res){
         // get all members
