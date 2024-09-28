5db6df289764a3c2764bf4d3de129845642fd044
er-priyanka <shahanepriyankas01@gmail.com>
2024-09-28 17:23:43 +0530

    user can get all profiles according to user gender

--- backend/Controllers/profile.controller.js
+++ backend/Controllers/profile.controller.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Profile = require('../Models/profile.model');
+const User = require('../Models/user.model');
 
 const profileController = {
     async addProfile(req, res){
@@ -7,6 +8,31 @@ const profileController = {
     },
     async getAllProfiles(req, res){
         // get all profile here
+
+        try{
+            const user = await User.findById(req.user.id);
+            // if user if admin
+            if(user.role == 'admin'){
+                const profiles = await Profile.find();
+                res.json(profiles);
+            }else{
+                let gender;
+                // if user is bride then she can see groom's profiles
+                // if user is groom then he can see bride's profiles
+                if(user.gender == 'female'){
+                    gender = "male";
+                }else{
+                    gender = "female";
+                }
+    
+                const profiles = await Profile.find(gender);
+    
+                res.json(profiles);
+            }  
+            
+        }catch(err){
+            res.status(500).json({error: err.message});
+        }
     },
     async getSingleProfile(req, res){
         // get single Profile by id here
