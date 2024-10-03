1f9898b61edf700f4508863728a77a757ea9bc5b
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-03 23:46:30 +0530

    added html content in content management

--- frontend/src/Components/Admin/AboutUs.jsx
+++ frontend/src/Components/Admin/AboutUs.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { MdAdd } from "react-icons/md";
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div>
+        {/* top section */}
+        <div className='p-6 flex justify-between items-center'>
+            <h1 className='text-left text-3xl font-bold'>About us</h1>
+            <div className='
+            flex gap-2
+            justify-center 
+            items-center 
+            border rounded-full
+            bg-red-700 hover:bg-red-600 text-white
+            py-4 px-6
+            cursor-pointer
+            text-[18px]
+            '>
+                {/* add icon */}
+                <MdAdd />
+                <p>Add Member</p>
+            </div>
+        </div>
+
+        {/* edit about us page */}
+        <div>
+            {/* edit button */}
+            <div>
+                <div>
+                    {/* edit icon */}
+                    <p>Edit</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <p>About website</p>
+                    <p>
+                        {/* about us info */}
+                    </p>
+                </div>
+
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
-export default AboutUs
+export default AboutUs;
---
+++ frontend/src/Components/Admin/HeadingAndparagraph.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeadingAndparagraph = () => {
+  return (
+    <div>HeadingAndparagraph</div>
+  )
+}
+
+export default HeadingAndparagraph
