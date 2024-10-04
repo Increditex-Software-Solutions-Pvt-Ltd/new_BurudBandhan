fad841838f5566796d3c4c1c411302161c36d867
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-04 22:05:52 +0530

    added css to table component

--- frontend/src/Components/Admin/MembersTable.jsx
+++ frontend/src/Components/Admin/MembersTable.jsx
@@ -13,40 +13,49 @@ const members = [
 
 const MembersTable = () => {
   return (
-    <table>
+    <div className='m-6'>
+        <table className='border text-left text-lg w-[100%]'>
             <thead>
-                <tr>
-                    <th>Action</th>
-                    <th>Member Photo</th>
-                    <th>Member Name</th>
-                    <th>Member Position</th>
+                <tr className='border-b text-sky-950 bg-gray-100'>
+                    <th className='p-4'>Action</th>
+                    <th className='p-4'>Member Photo</th>
+                    <th className='p-4'>Member Name</th>
+                    <th className='p-4'>Member Position</th>
                 </tr>
             </thead>
             <tbody>
                 {/* all members will see here */}
                 {
                     members.map((member, index)=>(
-                        <tr key={index}>
-                            <td>
-                                <div>
+                        <tr className='border-b' key={index}>
+                            <td className='p-4 flex items-center gap-4'>
+                                <div className='w-fit flex items-center gap-2
+                                 text-white bg-sky-400 hover:bg-sky-500 p-2 rounded-md
+                                    cursor-pointer' >
                                     <FaRegEdit />
                                     <p>Edit</p>
                                 </div>
-                                <div>
+                                <div
+                                className=' w-fit flex items-center gap-2
+                                text-white bg-red-400 hover:bg-red-500 p-2 rounded-md
+                                cursor-pointer'
+                                >
                                     <MdDeleteOutline />
                                     <p>Delete</p>
                                 </div>
                             </td>
-                            <td>
-                                <img src={member.image} alt="Member photo" />
+                            <td className='p-4'>
+                                <img className='w-[150px] h-[150px] object-cover border' src={member.image} alt="Member photo" />
                             </td>
-                            <td>{member.name}</td>
-                            <td>{member.position}</td>
+                            <td className='p-4'>{member.name}</td>
+                            <td className='p-4'>{member.position}</td>
                         </tr>
                     ))
                 }
             </tbody>
         </table>
+    </div>
+    
   )
 }
 
