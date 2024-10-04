e086d6749aae0cef463158e265d9ff56d0d1faf6
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-04 22:16:56 +0530

    added pagination

--- frontend/src/Components/Admin/Members.jsx
+++ frontend/src/Components/Admin/Members.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdOutlineSearch } from "react-icons/md";
 import MembersTable from './MembersTable';
 
 
 const Members = () => {
+    const [page, setPage] = useState(1);
+
+    const handleNextPage = ()=>{
+        setPage(prev=>prev+1);
+    };
+
+    const handlePrevPage = ()=>{
+        setPage(prev=>prev-1);
+    };
+
   return (
     <div>
         <h1 className='text-left text-3xl font-bold text-sky-950 p-6'>Members</h1>
@@ -30,6 +40,13 @@ const Members = () => {
 
         {/* all members are here table */}
         <MembersTable />
+
+        {/* pagination */}
+        <div className='m-6 p-4 flex justify-end items-center gap-2 '>
+            <button className='border bg-gray-200 hover:bg-gray-300 rounded-xl py-2 px-4' onClick={handlePrevPage}>Prev</button>
+            <button className='font-bold py-2 px-4'>{page}</button>
+            <button className='border bg-gray-200 hover:bg-gray-300 rounded-xl py-2 px-4' onClick={handleNextPage}>Next</button>
+        </div>
     </div>
   )
 }
