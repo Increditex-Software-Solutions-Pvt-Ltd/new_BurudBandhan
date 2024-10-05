9949e28f46b2773e2aebc57e4eec3d611adb6bea
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-05 18:36:38 +0530

    modified dashboard component

--- frontend/src/Components/Admin/Dashboard.jsx
+++ frontend/src/Components/Admin/Dashboard.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import Heading from './Heading';
 
+const data = [
+  {title:"Users created", total:8},
+  {title:"Profiles created", total:8},
+  {title:"Brides", total:8},
+  {title:"Grooms", total:8},
+]
+
 const Dashboard = () => {
   return (
     <div>
         <Heading innerText='Dashboard' />
+
+        {/* See relevant data here */}
+        <div className='m-6 flex justify-between items-center gap-[50px]'>
+            {
+              data.map((item, index)=>(
+                <div className='
+                w-1/4 p-4 py-14 
+                rounded-xl
+                cursor-pointer
+                bg-gradient-to-br from-red-700 to-red-400 hover:bg-gradient-to-tl
+                ' key={index}>
+                  <p className='text-2xl text-white'>{item.title}: <span>{item.total}</span></p>
+                </div>
+              ))
+            }
+        </div>
     </div>
   )
 }
