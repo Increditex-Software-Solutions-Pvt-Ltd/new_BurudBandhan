cfdac26f77d747bd0e341278582fec28e15b53a9
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-05 18:07:34 +0530

    modified users page

--- frontend/src/Components/Admin/Heading.jsx
+++ frontend/src/Components/Admin/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = ({innerText}) => {
   return (
     <div>
-        <h1 className='p-[20px] text-left text-4xl border-b border-b-red-200 font-bold text-gray-600 bg-red-50'>{innerText}</h1>
+        <h1 className='p-[20px] text-left text-3xl font-bold text-sky-950 '>{innerText}</h1>
 
     </div>
   )
--- frontend/src/Components/Admin/Users.jsx
+++ frontend/src/Components/Admin/Users.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import Heading from './Heading';
+import { MdOutlineSearch } from "react-icons/md";
 
 const Users = () => {
   return (
     <div>
-      <Heading innerText='Users' />
+      <Heading innerText="Users" />
+
+       {/* add filters */}
+       <div className='flex justify-between items-center m-6'>
+            {/* search here */}
+            <div className='w-1/4 flex items-center gap-4 p-2 text-lg border rounded-xl'>
+                <MdOutlineSearch className='text-gray-400 text-2xl' />
+                <input className='focus:outline-none' placeholder='search here' />
+            </div>
+
+            {/* select your position */}
+            <div className='w-1/4'>
+                <select>
+                    <option value="">All</option>
+                    <option value="">President</option>
+                    <option value="">Voice President</option>
+                    <option value="">Sachiv</option>
+                </select>
+            </div>
+            
+        </div>
     </div>
   )
 }
