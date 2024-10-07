7076c0823e3822726ab6b4ee983363c6f4c72fbf
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-07 15:06:15 +0530

    modified visibility of navbar and footer according to route path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
+import { useLocation } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 import AllRoutes from './Routes/AllRoutes';
 
 function App() {
+  const location = useLocation();
+  const hideHeaderFooterRoutes = ['/login', '/register', '/admin']; // Define paths where you don't want navbar/footer
+
   return (
     <div className="App">
-      <Navbar />
+      {!hideHeaderFooterRoutes.includes(location.pathname) && <Navbar />}
       <AllRoutes />
 
-      <Footer />
+      {!hideHeaderFooterRoutes.includes(location.pathname) && <Footer />}
     </div>
   );
 }
