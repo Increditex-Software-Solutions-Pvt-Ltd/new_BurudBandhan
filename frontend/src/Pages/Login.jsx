7471211b40c1e20ae0c7a67a054ce7f09d5cb764
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-09 20:01:38 +0530

    modified css

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -9,12 +9,11 @@ const Login = () => {
     <div className='bg-gray-50 py-[30px]'>
       {/* register container */}
       <div className='w-[80%] bg-white  m-auto shadow-xl  flex flex-col lg:flex-row'>
-        {/* this section is only visible on large screen */}
         <div className='lg:w-1/2'>
           {/* image */}
           <img className=' 
           w-[200px] h-[200px] rounded-full lg:rounded-none m-auto
-          lg:h-[100%] lg:w-full' src={marriageImage} alt="marriageImage" />
+          lg:h-[100vh] lg:w-full' src={marriageImage} alt="marriageImage" />
         </div>
 
         <div className='lg:w-1/2 p-[20px] py-[40px]'>
