d29bd2a04c0ad5787ae725873e8b1e301dd54319
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-09 19:49:42 +0530

    added responsiveness

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -4,17 +4,21 @@ import RegisterForm from '../Components/Register/RegisterForm';
 
 
 const Register = () => {
-  
+
   return (
     <div className='bg-gray-50 py-[30px]'>
       {/* register container */}
-      <div className='w-[80%] bg-white  m-auto shadow-xl min-h-[100vh] flex'>
-        <div className='w-1/2'>
+      <div className='w-[80%] bg-white  m-auto shadow-xl  flex flex-col lg:flex-row'>
+        {/* this section is only visible on large screen */}
+        <div className='lg:w-1/2'>
           {/* image */}
-          <img className='h-[100%] w-full' src={marriageImage} alt="marriageImage" />
+          <img className=' 
+          w-[200px] h-[200px] rounded-full lg:rounded-none m-auto
+          lg:h-[100%] lg:w-full' src={marriageImage} alt="marriageImage" />
         </div>
-        <div className='w-1/2 p-[20px] py-[40px]'>
-          <h1 className='font-bold text-2xl text-sky-950'>एक पाऊल सुयोग्य जोडीदाराच्या स्थळाकडे</h1>
+
+        <div className='lg:w-1/2 p-[20px] py-[40px]'>
+          <h1 className='font-bold text-xl lg:text-2xl text-sky-950'>एक पाऊल सुयोग्य जोडीदाराच्या स्थळाकडे</h1>
           <RegisterForm />
         </div>
       </div>
