cab02d800996b73b374e03c6e12cb6bb77282ff2
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-09 12:34:43 +0530

    modified admin navbar

--- frontend/src/Components/Admin/AdminNavbar.jsx
+++ frontend/src/Components/Admin/AdminNavbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import burudBandhan_logo from "../../Assets/burudBandhan_logo.jpeg";
 import { FaUserCircle } from "react-icons/fa";
+import ProfileModal from './ProfileModal';
 
 
 const AdminNavbar = () => {
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const modalRef = useRef(null); // Ref for the profile modal
+
+  const toggleProfileModal = (event) => {
+    event.stopPropagation();// Prevent the click event from bubbling up to document
+    setIsProfileOpen((prevState) => !prevState); // Toggle modal visibility
+  };
+ 
+
   return (
     <div className='py-2 px-10 border flex items-center justify-between relative'>
       <div className='w-fit  '>
@@ -12,25 +22,12 @@ const AdminNavbar = () => {
 
       <div>
         {/* profile icon here */}
-        <FaUserCircle className='text-5xl text-sky-700 cursor-pointer' />
+        <FaUserCircle
+        onClick={toggleProfileModal}
+         className='text-5xl text-sky-700 cursor-pointer' />
 
         {/* profile modal */}
-        <div className='w-[15%] p-4 text-left absolute top-[100%] right-[30px] bg-white shadow-xl rounded-lg'>
-            <div className='border-b py-2 font-semibold'>
-              <p>Admin full name</p>
-            </div>
-            <ul className='py-4 flex flex-col gap-4'>
-              <li>
-                <a href=''>Profile</a>
-              </li>
-              <li>
-                <a href=''>Setting</a>
-              </li>
-              <li>
-                <a href=''>Logout</a>
-              </li>
-            </ul>
-        </div>
+        {isProfileOpen && <ProfileModal modalRef={modalRef} />}
       </div>
 
       
