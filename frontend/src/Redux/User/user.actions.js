43e1bd73eb63388bee8dfa46c07fe0e5ffd1cd18
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-11 18:24:38 +0530

    added rest actions and switch cases in reducers which is related to user

--- frontend/src/Redux/User/user.actions.js
+++ frontend/src/Redux/User/user.actions.js
@@ -1,4 +1,4 @@
-import { LOGIN_SUCCESS, LOGIN_FAIL, REGISTER_SUCCESS, REGISTER_FAIL, LOGOUT } from "./user.types";
+import { LOGIN_SUCCESS, LOGIN_FAIL, REGISTER_SUCCESS, REGISTER_FAIL, LOGOUT, GET_USER_SUCCESS, GET_USER_FAIL, UPDATE_USER_SUCCESS, UPDATE_USER_FAIL, DELETE_USER_SUCCESS, DELETE_USER_FAIL, CHANGE_PASSWORD_SUCCESS, CHANGE_PASSWORD_FAIL } from "./user.types";
 import axios from 'axios';
 import api from '../api';
 
@@ -25,6 +25,51 @@ export const login = (userData) => async(dispatch) =>{
     }
 }
 
+// logout user
 export const logout = (dispatch)=>{
     dispatch({ type: LOGOUT });;
+}
+
+// get user info
+export const getUser = ()=>async(dispatch)=>{
+    try{
+        const res = await api.get('/user/auth/profile');
+        dispatch({type:GET_USER_SUCCESS, payload:res.data});
+
+    }catch(err){
+        dispatch({type:GET_USER_FAIL, payload:err.response?.data?.message || "Failed to fetch user data"});
+    }
+}
+
+
+// update user info
+export const updateUser = (userData)=>async (dispatch) => {
+    try{
+        const res = await api.patch('/user/auth/editprofile', userData);
+        dispatch({type:UPDATE_USER_SUCCESS, payload:res.data});
+
+    }catch(err){
+        dispatch({type:UPDATE_USER_FAIL, payload:err.response?.data?.message || "Failed to update user information"});
+    }
+}
+
+// delete user account
+export const deleteUser = ()=>async(dispatch)=>{
+    try{
+        const res = await api.delete('/user/auth/delete');
+        dispatch({type:DELETE_USER_SUCCESS, payload:res.data});
+    }catch(err){
+        dispatch({type:DELETE_USER_FAIL, payload:err.response?.data?.message || "Failed to delete user"});
+    }
+}
+
+
+// change password
+export const changePassword = (passwordData) => async(dispatch)=>{
+    try{
+        const res = await api.patch('/user/auth/change-password', passwordData);
+        dispatch({type:CHANGE_PASSWORD_SUCCESS, payload:res.data.message});
+    }catch(err){
+        dispatch({type:CHANGE_PASSWORD_FAIL, payload:err.response?.data?.message ||'Failed to change password'});
+    }
 }
--- frontend/src/Redux/User/user.reducers.js
+++ frontend/src/Redux/User/user.reducers.js
@@ -1,4 +1,4 @@
-import { LOGIN_SUCCESS, LOGIN_FAIL, REGISTER_SUCCESS, REGISTER_FAIL, LOGOUT } from "./user.types";
+import { LOGIN_SUCCESS, LOGIN_FAIL, REGISTER_SUCCESS, REGISTER_FAIL, LOGOUT, GET_USER_SUCCESS, GET_USER_FAIL, UPDATE_USER_SUCCESS, UPDATE_USER_FAIL, DELETE_USER_SUCCESS, DELETE_USER_FAIL, CHANGE_PASSWORD_SUCCESS, CHANGE_PASSWORD_FAIL } from "./user.types";
 
 
 const iniState = {
@@ -6,13 +6,13 @@ const iniState = {
     isAuthenticated:localStorage.getItem('token') ? true : false,
     loading:true,
     error:null,
-    user:null
+    user:null,
+    message:null
 }
 
 export const userReducer = (state=iniState, {type, payload})=>{
-    console.log(state);
     switch(type){
-        case LOGIN_SUCCESS:
+        // handle user registration
         case REGISTER_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
@@ -20,16 +20,125 @@ export const userReducer = (state=iniState, {type, payload})=>{
                 token:payload.token,
                 user:payload.user,
                 isAuthenticated:true,
-                loading:false
+                loading:false,
+                error:null
+            };
+        
+        case REGISTER_FAIL:
+            return {
+                ...state, 
+                token:null, 
+                isAuthenticated:false, 
+                loading:false, 
+                error:payload, 
+                user:null
+            };
+
+        // handle user login
+        case LOGIN_SUCCESS:
+            localStorage.setItem('token', payload.token);
+            return {
+                ...state,
+                token:payload.token,
+                user:payload.user,
+                isAuthenticated:true,
+                loading:false,
+                error:null
             };
         case LOGIN_FAIL:
-        case REGISTER_FAIL:
-            return {...state, token:null, isAuthenticated:false, loading:false, error:payload.message, user:null};
+            return {
+                ...state, 
+                token:null, 
+                isAuthenticated:false, 
+                loading:false, 
+                error:payload, 
+                user:null
+            };
+        // handle logout
         case LOGOUT:
           localStorage.removeItem('token');
-          return {...state, token:null, isAuthenticated:false, loading:false, error:null, user:null};
+          localStorage.removeItem('user');
+            return {
+                ...state, 
+                token:null, 
+                isAuthenticated:false, 
+                loading:false, 
+                error:null, 
+                user:null
+            };
         
+        // handle getting user data
+        case GET_USER_SUCCESS:
+            return {
+                ...state,
+                isAuthenticated:true,
+                loading: false,
+                user:payload,
+                error:null
+            };
         
+        case GET_USER_FAIL:
+            return {
+                ...state,
+                isAuthenticated:false,
+                loading:false,
+                user:null,
+                error:payload
+            };
+        
+        // handle updating user data
+        case UPDATE_USER_SUCCESS:
+            return {
+                ...state,
+                user:payload,
+                loading:false,
+                error:null
+            };
+        
+        case UPDATE_USER_FAIL:
+            return {
+                ...state,
+                loading:false,
+                error:payload
+            };
+        
+        // handle deleting user account
+        case DELETE_USER_SUCCESS:
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            return {
+                ...state,
+                token:null,
+                isAuthenticated:false,
+                loading:false,
+                user:null,
+                error:null
+            };
+        
+        case DELETE_USER_FAIL:
+            return{
+                ...state,
+                loading:false,
+                error:payload
+            };
+        
+        // handle password change
+        case CHANGE_PASSWORD_SUCCESS:
+            return {
+                ...state,
+                loading:false,
+                error:null,
+                message:payload
+            };
+        
+        case CHANGE_PASSWORD_FAIL:
+            return{
+                ...state,
+                loading:false,
+                error:payload
+            };
+        
+        // default state
           default:{
             return state;
         }
