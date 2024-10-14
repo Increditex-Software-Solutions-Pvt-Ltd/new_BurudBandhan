5af0e97f244b40e57935016325d6f52225626272
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-15 00:21:31 +0530

    added about us actions and it's types

---
+++ frontend/src/Redux/AboutUs/aboutus.actions.js
@@ -0,0 +1,43 @@
+import api from "../api"
+import { ADD_ABOUTUS_FAIL, ADD_ABOUTUS_SUCCESS, DELETE_ABOUTUS_FAIL, DELETE_ABOUTUS_SUCCESS, GET_ABOUTUS_FAIL, GET_ABOUTUS_SUCCESS, UPDATE_ABOUTUS_FAIL, UPDATE_ABOUTUS_SUCCESS } from "./aboutus.types";
+
+// add about us content
+export const addAboutUsContent = (content)=> async (dispatch) =>{
+    try{
+        const res = await api.post("/about-us/add", content);
+        dispatch({type:ADD_ABOUTUS_SUCCESS, payload:res.data});
+    }catch(err){
+        dispatch({type:ADD_ABOUTUS_FAIL, payload:err.response?.data?.message || "Failed to add about us content!"});
+    }
+}
+
+// get all about us content
+export const getAboutUsContent = () => async(dispatch)=>{
+    try{
+        const res = await api.get('/about-us');
+        dispatch({type:GET_ABOUTUS_SUCCESS, payload:res.data});
+    }catch(err){
+        dispatch({type:GET_ABOUTUS_FAIL, payload:err.response?.data?.message || "Failed to get about us content!"});
+    }
+}
+
+
+// update about us content
+export const updateAboutUsContent = (content) => async (dispatch)=>{
+    try{
+        const res = await api.patch(`/about-us`, content);
+        dispatch({type:UPDATE_ABOUTUS_SUCCESS, payload:res.data});
+    }catch(err){
+        dispatch({type:UPDATE_ABOUTUS_FAIL, payload:err.response?.data?.message || "Failed to update about us content!"});
+    }
+}
+
+// delete about us content
+export const deleteAboutUsContent = () => async (dispatch)=>{
+    try{
+        const res = await api.delete(`/about-us`);
+        dispatch({type:DELETE_ABOUTUS_SUCCESS, payload:res.data.message});
+    }catch(err){
+        dispatch({type:DELETE_ABOUTUS_FAIL, payload:err.response?.data?.message || "Failed to delete about us content!"});
+    }
+}
---
+++ frontend/src/Redux/AboutUs/aboutus.types.js
@@ -0,0 +1,11 @@
+export const ADD_ABOUTUS_SUCCESS = "aboutus/create_aboutus_success";
+export const ADD_ABOUTUS_FAIL = "aboutus/create_aboutus_fail";
+
+export const UPDATE_ABOUTUS_SUCCESS = "aboutus/update_aboutus_success";
+export const UPDATE_ABOUTUS_FAIL = "aboutus/update_aboutus_fail";
+
+export const GET_ABOUTUS_SUCCESS = "aboutus/get_aboutus_success";
+export const GET_ABOUTUS_FAIL = "aboutus/get_aboutus_fail";
+
+export const DELETE_ABOUTUS_SUCCESS = "aboutus/delete_aboutus_success";
+export const DELETE_ABOUTUS_FAIL = "aboutus/delete_aboutus_fail";
