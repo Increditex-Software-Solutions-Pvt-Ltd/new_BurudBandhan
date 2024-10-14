66d08fec2cb7a0559c38dbe7301d4efb9f52412a
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-14 17:12:05 +0530

    added success videos action types

---
+++ frontend/src/Redux/SuccessVideos/successVideos.types.js
@@ -0,0 +1,11 @@
+export const CREATE_SUCCESS_VIDEO_SUCCESS = "success_video/create_success_video_success";
+export const CREATE_SUCCESS_VIDEO_FAIL = "success_video/create_success_video_fail";
+
+export const UPDATE_SUCCESS_VIDEO_SUCCESS = "success_video/update_success_video_success";
+export const UPDATE_SUCCESS_VIDEO_FAIL = "success_video/update_success_video_fail";
+
+export const GET_SUCCESS_VIDEO_SUCCESS = "success_video/get_success_video_success";
+export const GET_SUCCESS_VIDEO_FAIL = "success_video/get_success_video_fail";
+
+export const DELETE_SUCCESS_VIDEO_SUCCESS = "success_video/delete_success_video_success";
+export const DELETE_SUCCESS_VIDEO_FAIL = "success_video/delete_success_video_fail";
