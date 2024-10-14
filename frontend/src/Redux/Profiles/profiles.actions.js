48a533c7e3f6168562e2920b1be5f8137aab7b0e
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-14 16:35:01 +0530

    added actions for profile

--- frontend/src/Redux/Profiles/profiles.actions.js
+++ frontend/src/Redux/Profiles/profiles.actions.js
@@ -1,5 +1,5 @@
 import api from "../api"
-import { CREATE_PROFILE_FAIL, CREATE_PROFILE_SUCCESS, GET_PROFILE_FAIL, GET_PROFILE_SUCCESS } from "./profile.types";
+import { CREATE_PROFILE_FAIL, CREATE_PROFILE_SUCCESS, DELETE_PROFILE_FAIL, DELETE_PROFILE_SUCCESS, GET_PROFILE_FAIL, GET_PROFILE_SUCCESS, UPDATE_PROFILE_FAIL, UPDATE_PROFILE_SUCCESS } from "./profile.types";
 
 // create user profile for marriage
 export const createUserProfile = (profile)=> async (dispatch) =>{
@@ -8,6 +8,16 @@ export const createUserProfile = (profile)=> async (dispatch) =>{
         dispatch({type:CREATE_PROFILE_SUCCESS, payload:res.data});
     }catch(err){
         dispatch({type:CREATE_PROFILE_FAIL, payload:err.response?.data?.message || "Failed to create profile!"});
+    }
+}
+
+// get all user marriage profile
+export const getAllUserProfile = () => async(dispatch)=>{
+    try{
+        const res = await api.get(``);
+        dispatch({type:GET_PROFILE_SUCCESS, payload:res.data});
+    }catch(err){
+        dispatch({type:GET_PROFILE_FAIL, payload:err.response?.data?.message || "Failed to create profile!"});
     }
 }
 
@@ -22,4 +32,21 @@ export const getUserProfile = (userId) => async(dispatch)=>{
 }
 
 // update user marriage profile by id
+export const updateUserProfile = (profileData) => async (dispatch)=>{
+    try{
+        const res = await api.patch("", profileData);
+        dispatch({type:UPDATE_PROFILE_SUCCESS, payload:res.data});
+    }catch(err){
+        dispatch({type:UPDATE_PROFILE_FAIL, payload:err.response?.data?.message || "Failed to update user marriage profile!"});
+    }
+}
+
 // delete user marriage profile by id
+export const deleteUserProfile = (userId) => async (dispatch)=>{
+    try{
+        const res = await api.delete(`${userId}`);
+        dispatch({type:DELETE_PROFILE_SUCCESS, payload:res.data.message});
+    }catch(err){
+        dispatch({type:DELETE_PROFILE_FAIL, payload:err.response?.data?.message || "Failed to delete profile!"});
+    }
+}
