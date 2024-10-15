6824742d025e9ba7dc8fb1647aeb1769388b142f
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-15 12:41:25 +0530

    added routes for about us content

---
+++ backend/Controllers/aboutus.controller.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const aboutusController = {
+    async addContent(req, res){
+        // add about us content
+    },
+    async getContent(req, res){
+        // get about us content
+    },
+    async updateContent(req, res){
+        // update content as per requirement
+    },
+    async deleteContent(req, res){
+        // delete content
+    }
+}
+
+
+module.exports = aboutusController;
---
+++ backend/Routes/aboutus.routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const auth = require('../Middlewares/auth');
+const aboutusController = require('../Controllers/aboutus.controller');
+
+const aboutusRouter = express.Router();
+
+
+// add content
+aboutusRouter.post('/add', auth, aboutusController.addContent);
+
+// get content
+aboutusRouter.get('/', auth, aboutusController.getContent);
+
+// update content
+aboutusRouter.patch('/', auth, aboutusController.updateContent);
+
+// delete content
+aboutusRouter.delete('/', auth, aboutusController.deleteContent);
+
+
