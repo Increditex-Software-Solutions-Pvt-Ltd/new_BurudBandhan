f282c3a5ea71384867f2c6c903b3a76a566a6672
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-24 19:06:57 +0530

    modified profiles not found component

--- frontend/src/Components/Profile/ProfilesNotFound.jsx
+++ frontend/src/Components/Profile/ProfilesNotFound.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { FaUserGroup } from "react-icons/fa6";
 
 const ProfilesNotFound = () => {
   return (
-    <div>ProfilesNotFound</div>
+    <div className='min-h-[500px] flex justify-center items-center'>
+      <div className=' text-orange-300  m-auto flex flex-col justify-center items-center'>
+        <FaUserGroup className='text-[200px]' />
+        <p className='text-2xl font-semibold'>Profiles Not Found</p>
+      </div>
+    </div>
   )
 }
 
