218af5973401b36f83d20a0cb72dce1c850ecd02
er-priyanka <shahanepriyankas01@gmail.com>
2024-10-26 13:25:15 +0530

    added css to search navbar

--- frontend/src/Components/Search/SearchNavbar.jsx
+++ frontend/src/Components/Search/SearchNavbar.jsx
@@ -10,12 +10,17 @@ const SearchNavbar = () => {
     border-b
     mb-[500px]
     '>
-        <div className='w-1/3 text-3xl text-sky-800'>
+        <div className='w-1/3 text-3xl text-sky-800  lg:hidden'>
             <MdMenu className='cursor-pointer' />
         </div>
-        <div className='w-[66%] flex items-center justify-between'>
+        <div className=' 
+        w-[66%] lg:w-full flex flex-col md:flex-row gap-[20px] 
+        items-center justify-between
+        lg:px-[100px]
+        xl:px-[250px]
+        '>
             <div className='
-            w-[40%]
+            xl:w-[40%]
             px-4 py-2
             flex 
             items-center 
@@ -25,7 +30,7 @@ const SearchNavbar = () => {
             text-[18px]
             '>
                 <MdSearch />
-                <input className=' w-full focus:outline-none px-2' type='text' placeholder='Search by name' />
+                <input className='w-full focus:outline-none px-2' type='text' placeholder='Search by name' />
             </div>
             <div className='flex items-center gap-6 text-white font-semibold'>
                 <button className='px-6 py-2 rounded-full bg-red-400 hover:bg-red-500'>All</button>
